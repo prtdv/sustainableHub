7ed3f9c35fe09776e2b75ca266aa1fe64b117f32
HarshilPahuja <141845326+HarshilPahuja@users.noreply.github.com>
2025-07-04 09:46:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@ import './index.css'
 import Navbar from './Components/home/nav.tsx'
 import LandingPage from './Components/home/landingpage.tsx'
 import Search from './Components/home/search.tsx'
-import { TableDemo } from './Components/home/demotable.tsx'
+import products from './products.ts'
+import Button from './Components/home/button.tsx'
+
+
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableCaption,
+} from "@/Components/ui/table" ;
+
 
 function App() {
   return (
@@ -10,7 +23,44 @@ function App() {
         <Navbar/>
         <LandingPage/>
         <Search/>
-        <TableDemo/>
+
+
+  <Table>
+  <TableCaption>Click for more Items</TableCaption>
+
+  <TableHeader>
+    <TableRow>
+      <TableHead>Product Image</TableHead>
+      <TableHead>Cost</TableHead>
+      <TableHead>Supplier</TableHead>
+      <TableHead>Score</TableHead>
+      <TableHead>Quantity</TableHead>
+    </TableRow>
+  </TableHeader>
+
+  <TableBody>
+
+    {products.map((product, index) => (
+
+    <TableRow key={index}>
+      <TableCell><img src={product.image} className="w-20 h-20 object-cover" /></TableCell>
+      <TableCell>{product.cost}</TableCell>
+      <TableCell>{product.supplier}</TableCell>
+      <TableCell>{product.score}</TableCell>
+      <TableCell>{product.quantity}</TableCell>
+    </TableRow>
+
+  ))}
+
+  </TableBody>
+  </Table>
+
+
+
+        <div className='flex justify-end  '>       
+          <Button className="" name="Add to cart"/>
+        </div>
+
     </body>
   )
 }
